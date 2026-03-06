f69d43c1c8509b1cd395d8d7aa1a4916cb0af56b
ILikeBread7 <pjaworski47@gmail.com>
2026-03-06 15:52:51 +0100

    Stuff

--- dev/parsers/example_sentences.js
+++ dev/parsers/example_sentences.js
@@ -2,8 +2,24 @@ import fs from 'node:fs'
 import * as readline from 'node:readline';
 import { stdin, stdout } from 'node:process';
 
+const LANGUAGE_PARAMS = [ '--lang', '-l' ];
+const args = process.argv.slice(2);
+const files = [];
+let languageFile = '';
+for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (LANGUAGE_PARAMS.includes(arg)) {
+        i++;
+        languageFile = args[i];
+        continue;
+    }
+    files.push(arg);
+}
+
+const words = fs.readFileSync(languageFile, 'utf8').split('\n');
+const wordsFrequencyMap = new Map(words.map((word, index) => [ word, index + 1 ]));
 let text = '';
-process.argv.slice(2).forEach(filename => {
+files.forEach(filename => {
     const fileText = fs.readFileSync(filename, 'utf8');
     text += preprocessText(fileText) + '\n';
 });
@@ -11,14 +27,41 @@ const sentences = text.split('\n');
 
 const read = readline.createInterface({ input: stdin, output: stdout });
 read.on('line', word => {
-    sentences.forEach(sentence => processSentence(word, sentence));
+    console.log(` --- ${word}:`);
+    sentences
+        .map(sentence => [ processSentence(word, sentence), sentence ])
+        .filter(([ score ]) => score > 0)
+        .sort((x1, x2) => x1[0] - x2[0])
+        .slice(0, 5)
+        .forEach(([ , sentence ]) => console.log(sentence));
 });
 
+/**
+ * 
+ * @param {string} word 
+ * @param {string} sentence 
+ * @returns {number} score, 0 if not applicable
+ */
 function processSentence(word, sentence) {
-    const split = sentence.split(/\s|,|[.]{3}|"|—|'/);
-    if (split.includes(word)) {
-        console.log(sentence);
+    sentence = sentence.toLowerCase();
+    word = word.toLowerCase();
+
+    if (!sentence.includes(word)) {
+        return 0;
     }
+
+    if (sentence.includes(' ')) {
+        const split = sentence.split(/\s|,|[.]{3}|"|—|'/);
+        if (!split.includes(word)) {
+            return 0;
+        }
+        return split.reduce(
+            (acc, currentWord) => acc + (word === currentWord ? 0 : (wordsFrequencyMap.get(currentWord) || (wordsFrequencyMap.size + 1))),
+            0
+        );
+    }
+
+    return 0;
 }
 
 function preprocessText(text) {
