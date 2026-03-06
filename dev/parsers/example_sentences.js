5fbbd00b7010d16f9905dd42158a25f04b38d551
ILikeBread7 <pjaworski47@gmail.com>
2026-03-06 16:25:02 +0100

    Stuff + change quotes to single quotes

--- dev/parsers/example_sentences.js
+++ dev/parsers/example_sentences.js
@@ -21,7 +21,8 @@ const wordsFrequencyMap = new Map(words.map((word, index) => [ word, index + 1 ]
 let text = '';
 files.forEach(filename => {
     const fileText = fs.readFileSync(filename, 'utf8');
-    text += preprocessText(fileText) + '\n';
+    const preprocessFunction = fileText.endsWith('.xml') ? preprocessXml : preprocessTxt;
+    text += preprocessFunction(fileText) + '\n';
 });
 const sentences = text.split('\n');
 
@@ -64,22 +65,21 @@ function processSentence(word, sentence) {
     return 0;
 }
 
-function preprocessText(text) {
-    text = removeFluff(text);
-    return text;
+function preprocessXml(text) {
+    text = text.replaceAll(/<text\b[^>]*>([\s\S]*?)<\/text>/g, '$1');
 }
 
-function removeFluff(text) {
+function preprocessTxt(text) {
     // remove [] references, headings, and empty lines
     text = text.replaceAll(/\[(\d|[a-z])+\]/g, '')
         .replaceAll(/^.*(?<![.!?])\n?$/gm, '')
         .replaceAll(/^\s+/gm, '');
 
     // protect ellipsis
-    text = text.replaceAll(/\.\.\./g, "§ELLIPSIS§");
+    text = text.replaceAll(/\.\.\./g, '§ELLIPSIS§');
 
     // protect common abbreviations
-    text = text.replaceAll(/\b(Mr|Mrs|Ms|Dr|Prof|Sr|Jr|St|vs|etc|e\.g|i\.e)\./g, "$1§DOT§");
+    text = text.replaceAll(/\b(Mr|Mrs|Ms|Dr|Prof|Sr|Jr|St|vs|etc|e\.g|i\.e)\./g, '$1§DOT§');
 
     // protect parentheses
     const parens = [];
@@ -89,21 +89,13 @@ function removeFluff(text) {
     });
 
     // split
-    text = text.replaceAll(/([.!?])\s+/g, "$1\n");
+    text = text.replaceAll(/([.!?])\s+/g, '$1\n');
 
     // restore
-    text = text.replaceAll(/§ELLIPSIS§/g, "...");
-    text = text.replaceAll(/§DOT§/g, ".");
+    text = text.replaceAll(/§ELLIPSIS§/g, '...');
+    text = text.replaceAll(/§DOT§/g, '.');
     text = text.replaceAll(/§PAREN(\d+)§/g, (_, i) => parens[i]);
 
     return text;
-
-
-    return text
-        .replaceAll(/\[(\d|[a-z])+\]/g, '')
-        .replaceAll(/^.*(?<![.!?])\n?$/gm, '')
-        .replaceAll(/^\s+/gm, '')
-        .replaceAll(/([.!?]) (?=[^()]*?(?:\(|$))/g, '$1\n')
-        .replaceAll(/ [.]{3}\n/g, ' ... ');
 }
 
