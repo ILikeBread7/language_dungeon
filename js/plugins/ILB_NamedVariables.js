89d47f6b64aacd03a70b8f759c0d0e755f9e58da
ILikeBread7 <pjaworski47@gmail.com>
2026-03-16 16:49:43 +0100

    WIP Named variables

---
+++ js/plugins/ILB_NamedVariables.js
@@ -0,0 +1,138 @@
+//=============================================================================
+// ILB_NamedVariables.js
+//=============================================================================
+
+/*:
+ * @plugindesc Exposes global objects $nv $ns $nce for named variables, switches and common events respectively
+ *
+ * @author I_LIKE_BREAD7
+ *
+ * @help
+ * This plugin allows you to use game variables and switches in scripts using their names.
+ * Example: $nv.testVariable instead of $gameVariables.value(1)
+ */
+
+var $nv = {};
+var $ns = {};
+var $nce = {};
+
+(() => {
+
+    const DEFAULT_VALUE_SPLIT_CHAR = ':';
+    const defaultVariableValues = [];
+    const defaultSwitchValues = [];
+
+    fetch('../data/System.json')
+        .then(response => response.json())
+        .then(system => {
+            system.variables.forEach((name, index) => {
+                if (!name) {
+                    return;
+                }
+
+                const split = splitOnce(name, DEFAULT_VALUE_SPLIT_CHAR);
+                name = split[0].trim();
+                const defaultValue = split[1];
+
+                if ($nv.hasOwnProperty(name)) {
+                    return;
+                }
+                if (defaultValue !== undefined) {
+                    defaultVariableValues[index] = defaultValue.trim();
+                }
+
+                Object.defineProperty($nv, name, {
+                    get() {
+                        return $gameVariables.value(index);
+                    },
+                    set(value) {
+                        $gameVariables.setValue(index, value);
+                    }
+                });
+            });
+
+            system.switches.forEach((name, index) => {
+                if (!name) {
+                    return;
+                }
+
+                const split = splitOnce(name, DEFAULT_VALUE_SPLIT_CHAR);
+                name = split[0].trim();
+                const defaultValue = split[1];
+
+                if ($ns.hasOwnProperty(name)) {
+                    return;
+                }
+                if (defaultValue !== undefined) {
+                    defaultSwitchValues[index] = JSON.parse(defaultValue);
+                }
+
+                Object.defineProperty($ns, name, {
+                    get() {
+                        return $gameSwitches.value(index);
+                    },
+                    set(value) {
+                        $gameSwitches.setValue(index, value);
+                    }
+                });
+            });
+        });
+
+    fetch('../data/CommonEvents.json')
+        .then(response => response.json())
+        .then(commonEvents => {
+            commonEvents.forEach(commonEvent => {
+                const { name, id } = commonEvent || {};
+                if (!name || $nce.hasOwnProperty(name)) {
+                    return;
+                }
+
+                Object.defineProperty($nce, name, {
+                    get() {
+                        return id;
+                    }
+                });
+            });
+        });
+
+    Game_Variables.prototype.value = function(variableId) {
+        const value = this._data[variableId];
+        if (value === undefined) {
+            const defaultValueString = defaultVariableValues[variableId];
+            if (defaultValueString) {
+                const defaultValue = JSON.parse(defaultValueString);
+                this._data[variableId] = defaultValue;
+                return defaultValue;
+            }
+        }
+        return value || 0;
+    };
+
+    Game_Variables.prototype.setValue = function(variableId, value) {
+        if (variableId > 0 && variableId < $dataSystem.variables.length) {
+            this._data[variableId] = value;
+            this.onChange();
+        }
+    };
+
+    Game_Switches.prototype.value = function(switchId) {
+        const value = this._data[switchId];
+        if (value === undefined) {
+            const defaultValue = defaultSwitchValues[switchId];
+            if (defaultValue !== undefined) {
+                return this._data[switchId] = !!defaultValue;
+            }
+        }
+        return !!value;
+    };
+
+    function splitOnce(string, splitChar) {
+        const index = string.indexOf(splitChar);
+        if (index < 0) {
+            return [ string ];
+        }
+
+        return [ string.substring(0, index), string.substring(index + 1, string.length) ];
+    }
+
+})();
