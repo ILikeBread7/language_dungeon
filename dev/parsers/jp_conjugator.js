48852d46e1e59a950906a5502530a4fc528fb972
ILikeBread7 <pjaworski47@gmail.com>
2026-03-27 17:50:41 +0100

    WIP JP conjugator

---
+++ dev/parsers/jp_conjugator.js
@@ -0,0 +1,93 @@
+import * as codec from 'kamiya-codec';
+import fs from 'node:fs';
+
+const TYPE = Object.freeze({
+    UNKNOWN: 0,
+    ICHIDAN_VERB: 1,
+    I_ADJECTIVE: 2,
+    NA_ADJECTIVE: 3,
+    NOUN: 4,
+    GODAN_VERB: 5
+});
+
+const dict = JSON.parse(fs.readFileSync('dicts/JMdict_e.json', 'utf8'));
+const dictMap = new Map(
+    dict.JMdict.entry.flatMap(word => {
+        let element = word['k_ele'];
+        if (!element) {
+            element = word['r_ele'];
+            if (!element) {
+                return;
+            }
+        }
+
+        const elements = elementToArray(element);
+        const types = elementToArray(word['sense']).flatMap(senses => elementToArray(senses).flatMap(sense => elementToArray(sense.pos).map(descriptionToType)));
+        return elements.map(element => [ element['keb'] || element['reb'], [...new Set(types)].filter(Boolean) ]);
+    })
+    .filter(Boolean)
+);
+
+['悲しい', '綺麗', '変える', '帰る', '選択', 'ああ'].forEach(word => {
+    console.log(word);
+    const types = dictMap.get(word);
+    types.forEach(type => {
+        console.log(type);
+        if ([TYPE.I_ADJECTIVE, TYPE.NA_ADJECTIVE].includes(type)) {
+            console.log(
+                codec.adjConjugations.map(conjugation => codec.adjConjugate(word, conjugation, type === TYPE.I_ADJECTIVE))
+            );
+        }
+
+        if ([TYPE.GODAN_VERB, TYPE.ICHIDAN_VERB].includes(type)) {
+            console.log(
+                codec.conjugations.map(conjugation => codec.conjugate(word, conjugation, type === TYPE.ICHIDAN_VERB))
+            );
+        }
+    });
+})
+
+
+// console.log(
+//     [...new Set(codec.conjugations.flatMap(conjugation => codec.conjugate('書く', conjugation)))].sort((a, b) => b.length - a.length).join('\n')
+// );
+// console.log(
+//     [...new Set(codec.adjConjugations.flatMap(conjugation => codec.adjConjugate('新しい', conjugation, true)))].sort((a, b) => b.length - a.length).join('\n')
+// );
+// console.log(
+//     [...new Set(codec.adjConjugations.flatMap(conjugation => codec.adjConjugate('綺麗', conjugation, false)))].sort((a, b) => b.length - a.length).join('\n')
+// );
+
+function descriptionToType(description) {
+    description = description.toLowerCase();
+    
+    if (description.includes('noun (common) (futsuumeishi)')) {
+        return TYPE.NOUN;
+    }
+
+    if (description.includes('adjective (keiyoushi)')) {
+        return TYPE.I_ADJECTIVE;
+    }
+
+    if (description.includes('adjectival nouns or quasi-adjectives (keiyodoshi)')) {
+        return TYPE.NA_ADJECTIVE;
+    }
+
+    if (description.includes('ichidan verb')) {
+        return TYPE.ICHIDAN_VERB;
+    }
+
+    if (description.includes('godan verb')) {
+        return TYPE.GODAN_VERB;
+    }
+
+    return TYPE.UNKNOWN;
+}
+
+function elementToArray(element) {
+    if (Array.isArray(element)) {
+        return element;
+    }
+
+    return [ element ];
+}
