d3130ba780831ca9e705f2ea95a05d8d9a23dd83
ILikeBread7 <pjaworski47@gmail.com>
2026-04-14 16:57:59 +0200

    PArams parser test

---
+++ dev/parsers/sentences_manager.js
@@ -0,0 +1,11 @@
+import { parseArgv } from './utils.js';
+
+const params = parseArgv(process.argv, {
+    test1: { longName: 'test1', shortName: '1', defaultValue: 0, mapper: Number },
+    test2: { longName: 'test2', shortName: '2', defaultValue: 0, mapper: (a, b) => Number(a) + Number(b)},
+    test3: { shortName: '3' },
+    test4: { shortName: '4' },
+    test5: { longName: 'test5', shortName: '5' },
+});
+
+Object.entries(params).forEach(([ key, value ]) => console.log(key, value));
