0f62e4f6724ada1a95ce540409186269685f6ab2
ILikeBread7 <pjaworski47@gmail.com>
2026-04-21 16:34:49 +0200

    Sntences explainer found auxiliaries

--- dev/parsers/sentences_explainer.js
+++ dev/parsers/sentences_explainer.js
@@ -2,13 +2,10 @@ import fs from 'node:fs'
 import { TokenizerBuilder } from 'lindera-wasm-unidic-nodejs';
 import { parseArgv } from './utils.js';
 
-const SENTENCES_ARGS = [ '--sentences', '-s' ];
-const LANGUAGE_CODE_ARGS = [ '--lang-code', '-lc' ];
-const PROD_ARGS = [ '--prod', '-p' ];
-
 const params = parseArgv(process.argv, {
     sentencesFile: { longName: 'sentences', shortName: 's', required: true, mapper: String },
-    prod: { longName: 'prod', shortName: 'p' }
+    prod: { longName: 'prod', shortName: 'p' },
+    auxiliaries: { longName: 'auxiliaries', shortName: 'a' }
 });
 
 const UNIDIC_JOIN_SUFFIXES = [ '助動詞', '接尾辞' ];
@@ -26,19 +23,31 @@ const tokenizerBuilder = new TokenizerBuilder();
 tokenizerBuilder.setDictionary('embedded://unidic');
 const tokenizer = tokenizerBuilder.build();
 
+const foundAuxiliaries = new Map();
+function addFoundAuxiliary(wordKey, sentence) {
+    if (foundAuxiliaries.has(wordKey)) {
+        return;
+    }
+    foundAuxiliaries.set(wordKey, sentence);
+}
+
 const wordsWithSentences = JSON.parse(fs.readFileSync(params.sentencesFile, 'utf8'));
 Object.keys(wordsWithSentences).forEach(word => wordsWithSentences[word] = wordsWithSentences[word].map(explainSentence));
-console.log(JSON.stringify(wordsWithSentences, null, jsonConfig.spaces));
+if (params.auxiliaries) {
+    foundAuxiliaries.entries().forEach(([ wordKey, sentence ]) => console.log(`${wordKey}: ${sentence}`));
+} else {
+    console.log(JSON.stringify(wordsWithSentences, null, jsonConfig.spaces));
+}
 
 function explainSentence(sentence) {
     const tokens = tokenizer.tokenize(sentence);
-    const words = splitIntoWordsWithAuxiliaries(tokens);
+    const words = splitIntoWordsWithAuxiliaries(tokens, sentence);
     return words
         .map(mapWordDataToText)
         .join('');
 }
 
-function splitIntoWordsWithAuxiliaries(tokens) {
+function splitIntoWordsWithAuxiliaries(tokens, sentence) {
     const words = [];
 
     for (const token of tokens) {
@@ -53,7 +62,9 @@ function splitIntoWordsWithAuxiliaries(tokens) {
                 const auxiliaryForm = form === UNIDIC_BLANK_FORM
                     ? findMostDetailedPos(token)
                     : form;
-                modifiedWord.parts.push(mapBaseWordWithFormToText(base, auxiliaryForm));
+                const wordKey = mapBaseWordWithFormToText(base, auxiliaryForm);
+                addFoundAuxiliary(wordKey, sentence)
+                modifiedWord.parts.push(wordKey);
                 modifiedWord.word += word;
                 continue;
             }
