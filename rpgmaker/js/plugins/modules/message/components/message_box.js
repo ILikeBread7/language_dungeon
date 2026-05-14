96078358614294db4b4a97079e245d01c45329f9
ILikeBread7 <pjaworski47@gmail.com>
2026-05-14 19:13:49 +0200

    WIP displaying chars one at a time

--- rpgmaker/js/plugins/modules/message/components/message_box.js
+++ rpgmaker/js/plugins/modules/message/components/message_box.js
@@ -1,5 +1,7 @@
 const BOX_HEIGHT = '25vh';
 const HIDDEN_TOP = '100vh';
+const TRANSITION_TIME = '1s';
+const CHAR_WRITE_WAIT = 200;
 
 export class MessageBox extends HTMLElement {
 
@@ -22,11 +24,15 @@ export class MessageBox extends HTMLElement {
                 color: #ffffff;
                 position: absolute;
                 top: ${HIDDEN_TOP};
-                transition: top 2s;
+                transition: top ${TRANSITION_TIME};
+                white-space: pre;
             }
         `;
         this.shadowRoot.append(style, messageContainer);
         this._messageContainer = messageContainer;
+        this._messageTextBuffer = [];
+        this._messageTextHtmlTagStack = [ messageContainer ];
+        this._messageTextDisplayImmediately = false;
     }
 
     async messageBoxShow() {
@@ -63,6 +69,79 @@ export class MessageBox extends HTMLElement {
      */
     messageBoxSetText(text) {
         this._messageContainer.innerHTML = text;
+    }
+
+    /**
+     * 
+     * @param {string} text May include html
+     * @description Displays the text one character at a time
+     */
+    async messageBoxDisplayText(text) {
+        const split = this._splitTextForDisplay(text);
+        for (const textPiece of split) {
+            if (this._isHtmlOpeningTag(textPiece)) {
+                const element = this._createElementFromHtml(textPiece);
+                const currentTopElement = this._messageTextHtmlTagStack[this._messageTextHtmlTagStack.length - 1];
+                currentTopElement.appendChild(element);
+                this._messageTextHtmlTagStack.push(element);
+            } else if (this._isHtmlClosingTag(textPiece)) {
+                if (this._messageTextHtmlTagStack.length === 1) {
+                    console.warn(`Closing html tag when no tag is opened!`);
+                } else {
+                    this._messageTextHtmlTagStack.pop();
+                }
+            } else {
+                if (!this._messageTextDisplayImmediately) {
+                    await new Promise(resolve => setTimeout(resolve, CHAR_WRITE_WAIT));
+                }
+                const currentTopElement = this._messageTextHtmlTagStack[this._messageTextHtmlTagStack.length - 1];
+                currentTopElement.innerHTML += textPiece;
+            }
+        }
+
+        // If there are some leftover open html tags remove them
+        if (this._messageTextHtmlTagStack.length !== 1) {
+            this._messageTextHtmlTagStack.splice(1);
+        }
+        this._messageTextDisplayImmediately = false;
+    }
+
+    messageBoxDisplayImmediately() {
+        this._messageTextDisplayImmediately = true;
+    }
+
+    /**
+     * 
+     * @param {string} text May include html
+     */
+    _splitTextForDisplay(text) {
+        return text.match(/<.*?>|.|\n/g);  // Split individual characters, but keep html tags intact
+    }
+
+    /**
+     * 
+     * @param {string} text 
+     */
+    _isHtmlOpeningTag(text) {
+        return text[0] === '<' && text[1] !== '/';
+    }
+
+    /**
+     * 
+     * @param {string} text 
+     */
+    _isHtmlClosingTag(text) {
+        return text.startsWith('</');
+    }
+
+    /**
+     * 
+     * @param {string} html 
+     */
+    _createElementFromHtml(html) {
+        const template = document.createElement('template');
+        template.innerHTML = html.trim();
+        return template.content.firstElementChild;
     }
 
 }
--- rpgmaker/js/plugins/modules/message/message.js
+++ rpgmaker/js/plugins/modules/message/message.js
@@ -4,11 +4,18 @@ console.log('messagw!');
 const box = new MessageBox();
 document.body.appendChild(box);
 
-async function addText(text) {
+async function setText(text) {
     box.messageBoxSetText(text);
     await box.messageBoxShow();
-    await box.messageBoxHide();
+    // await box.messageBoxHide();
 }
 
-setTimeout(() => addText('Test123!!!'), 1000)
+async function displayText(text) {
+    await box.messageBoxShow();
+    setTimeout(() => box.messageBoxDisplayImmediately(), 1000)
+    await box.messageBoxDisplayText(text);
+    await box.messageBoxDisplayText('\n' + text);
+}
+
+setTimeout(() => displayText(/*html*/`Test123!!! <span style="color:green">GREEN</span> Text!!!`));
 
