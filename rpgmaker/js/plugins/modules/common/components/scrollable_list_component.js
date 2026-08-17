77111cc731f9c182c5e87645119702268da8480a
ILikeBread7 <pjaworski47@gmail.com>
2026-08-17 18:44:59 +0200

    Fixed? next / previous page

--- rpgmaker/js/plugins/modules/common/components/scrollable_list_component.js
+++ rpgmaker/js/plugins/modules/common/components/scrollable_list_component.js
@@ -60,18 +60,6 @@ export class ScrollableListComponent extends ChoicesListComponent {
     }
 
     scrollableListNextPage() {
-        this._changePage(element => element.nextElementSibling);
-    }
-
-    scrollableListPreviousPage() {
-        this._changePage(element => element.previousElementSibling);
-    }
-
-    /**
-     * 
-     * @param {(current: HTMLElement) => HTMLElement} nextElementFunction 
-     */
-    _changePage(nextElementFunction) {
         const currentOption = this.choicesListCurrentlySelectedOption;
         if (!currentOption) {
             return;
@@ -83,17 +71,81 @@ export class ScrollableListComponent extends ChoicesListComponent {
         for (
             let element = currentOption.option.element;
             element;
-            element = nextElementFunction(element)
+            element = element.nextElementSibling
         ) {
             const optionDimensions = element.getBoundingClientRect();
-            if (this._isElementOutOfView(optionDimensions, listDimensions, containerDimensions)) {
-                const maxScroll = this._calculateMaxScroll(listDimensions.height, containerDimensions.height);
-                this._scrollTo(optionDimensions.top, maxScroll);
-                const optionIndex = Number(element.dataset.index);
-                super.choicesListSelectOption(optionIndex);
+            if (this._isElementBelowContainer(optionDimensions, containerDimensions)) {
+                const index = Number(element.dataset.index);
+                this.choicesListSelectOption(index);
                 return;
             }
         }
+
+        this.choicesListSelectOption(0);
+    }
+
+    scrollableListPreviousPage() {
+        const currentOption = this.choicesListCurrentlySelectedOption;
+        if (!currentOption) {
+            return;
+        }
+
+        const listDimensions = this._list.getBoundingClientRect();
+        const containerDimensions = this._container.getBoundingClientRect();
+
+        for (
+            let element = currentOption.option.element;
+            element;
+            element = element.previousElementSibling
+        ) {
+            const optionDimensions = element.getBoundingClientRect();
+            if (this._isElementAboveContainer(optionDimensions, containerDimensions)) {
+                const index = Number(element.dataset.index);
+                this.choicesListSelectOptionNoEvent(index);
+                this._selectTopmostVisibleElement(element, containerDimensions);
+                return;
+            }
+        }
+
+        this.choicesListSelectOptionNoEvent(this.choicesListDisplayedOptions.length - 1);
+        const startingElement = this.choicesListCurrentlySelectedOption.option.element;
+        this._selectTopmostVisibleElement(startingElement, containerDimensions);
+    }
+
+    /**
+     * 
+     * @param {HTMLElement} startingElement 
+     * @param {DOMRect} containerDimensions
+     */
+    _selectTopmostVisibleElement(startingElement, containerDimensions) {
+        const topmostElement = this._findTopmostVisibleElement(startingElement, containerDimensions);
+        const topmostIndex = Number(topmostElement.dataset.index);
+        super.choicesListSelectOption(topmostIndex);
+    }
+
+    /**
+     * 
+     * @param {HTMLElement} startingElement 
+     * @param {DOMRect} containerDimensions
+     */
+    _findTopmostVisibleElement(startingElement, containerDimensions) {
+        for (
+            let element = startingElement;
+            element;
+            element = element.previousElementSibling
+        ) {
+            const previousElement = element.previousElementSibling;
+            if (!previousElement) {
+                return element;
+            }
+
+            const previousElementDimensions = previousElement.getBoundingClientRect();
+            if (this._isElementAboveContainer(previousElementDimensions, containerDimensions)) {
+                return element;
+            }
+        }
+
+        return startingElement;
     }
 
     /**
